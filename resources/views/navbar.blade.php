1f32a481f3d43e0aaf4b56d8182d985fbdf2cc40
Tegar Dwi Vantoro <tegardvz@gmail.com>
2024-05-13 23:37:19 +0700

    logout and redirect

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -21,7 +21,10 @@
                         <li><a class="dropdown-item" href="#">Profile</a></li>
                         <li><a class="dropdown-item" href="#">Settings</a></li>
                         <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="/logout">Log Out</a></li>
+                        <li><a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Log Out</a></li>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
                     </ul>
                 </div>
             @endif
