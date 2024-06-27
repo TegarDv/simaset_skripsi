480aebc643f4d76a69cfadb66033cf749dc2ba74
Tegar Dwi Vantoro <tegardvz@gmail.com>
2024-06-27 19:42:15 +0700

    transaksi peminjaman

--- resources/views/transaksi_peminjaman/index.blade copy.php
+++ resources/views/transaksi_peminjaman/index.blade copy.php
@@ -1,67 +1,8 @@
-@extends('template.app')
-
-@section('title', 'Transaksi Peminjaman')
-
-@section('content')
-<h4 class="text-start">Transaksi Peminjaman</h4>
-<div class="card shadow-lg">
-    <h5 class="card-header">Data Transaksi</h5>
-    <div class="card-body">
-        <div class="alert alert-info d-flex align-items-center" style="color: #000;"  role="alert">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-diamond-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
-                <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-            </svg>
-            <div>
-                Berisi data transaksi peminjaman aset.
-            </div>
-        </div>
-    </div>
-    <div class="card-datatable table-responsive">
-        <div class="ms-3">
-            <button class="btn btn-primary create-btn">
-                <i class="ti ti-plus me-sm-1"></i><span class="d-none d-sm-inline-block">Tambah Data</span>
-            </button>
-            <button id="reloadDatatable" class="btn btn-primary">
-                <i class="bi bi-arrow-clockwise me-sm-1"></i> <span class="d-none d-sm-inline-block">Reload Data</span>
-            </button>
-        </div>
-        <table class="table table-bordered text-light" style="min-width: 100%;" id="data_trxs"></table>
-    </div>
-</div>
-
-{{-- Loader modal --}}
-<div class="loader-modal" id="loader-modal">
-    <div class="loader"></div>
-</div>
-
-{{-- Add Modal --}}
-<div class="modal fade" id="createModal" tabindex="-1" aria-labelledby="addModalLabel" aria-hidden="true" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1">
-    <div class="modal-dialog modal-xl modal-dialog-scrollable">
-        <div class="modal-content">
-            <!-- Modal content will be loaded here -->
-        </div>
-    </div>
-</div>
-
-<!-- Detail modal container -->
-<div class="modal fade" id="detailModal" tabindex="-1" aria-labelledby="detailModalLabel" aria-hidden="true" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1">
-    <div class="modal-dialog modal-xl modal-dialog-scrollable">
-        <div class="modal-content">
-            <!-- Modal content will be loaded here -->
-        </div>
-    </div>
-</div>
-
-{{-- Edit Modal --}}
-<div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1">
-    <div class="modal-dialog modal-xl modal-dialog-scrollable">
-        <div class="modal-content">
-            <!-- Modal content will be loaded here -->
-        </div>
-    </div>
-</div>
-@endsection
-@push('css')
+@extends('layouts')
+@section('title', 'SIMASET - Transaksi Masuk')
+@section('custom_css')
+<link rel="stylesheet" href="{{ asset('assets/DataTables/datatables.min.css') }}" />
+<link rel="stylesheet" href="{{ asset('assets/DataTables/DataTables-1.13.6/css/dataTables.bootstrap5.min.css') }}" />
 <style>
     .loader-modal {
         position: fixed;
@@ -107,15 +48,85 @@
         }
     }
 </style>
-@endpush
-@push('cssvendor')
-    <link rel="stylesheet" href="{!! asset('assets') !!}/vendor/libs/datatables-bs5/datatables.bootstrap5.css" />
-    <link rel="stylesheet" href="{!! asset('assets') !!}/vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.css" />
-    <link rel="stylesheet" href="{!! asset('assets') !!}/vendor/libs/datatables-checkboxes-jquery/datatables.checkboxes.css" />
-    <link rel="stylesheet" href="{!! asset('assets') !!}/vendor/libs/datatables-buttons-bs5/buttons.bootstrap5.css" />
-@endpush
+@endsection
+@section('content')
+{{-- Header --}}
+<section id="base">
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col">
+                <p>
+                    <h2 class="text-start">Transaksi - Peminjaman Aset</h2>
+                    <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam, eaque.</h5>
+                </p>
+            </div>
+        </div>
 
-@push('js')
+        <div class="container rounded p-1">
+            <div class="card shadow-lg">
+                <h5 class="card-header text-warning-emphasis">Data Transaksi</h5>
+                <div class="card-body">
+                    @if(session()->has('success'))
+                        <div class="alert alert-success">
+                            {{ session('success') }}
+                        </div>
+                    @elseif(session()->has('error'))
+                        <div class="alert alert-danger">
+                            {{ session('error') }}
+                        </div>
+                    @endif
+                    <div class="table-container">
+                        <!-- loader -->
+                        <div class="loader-modal" id="loader-modal">
+                            <div class="loader"></div>
+                        </div>
+
+                        <div class="m-3">
+                            <button class="btn btn-primary create-btn">Tambah</button>
+                            <button id="reloadDatatable" class="btn btn-primary">Reload Data</button>
+                        </div>
+        
+                        <div class="table-responsive">
+                            <table class="table table-bordered text-light" style="min-width: 100%;" id="data_trxs"></table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        {{-- Close Card --}}
+
+        {{-- Add Modal --}}
+        <div class="modal fade" id="createModal" tabindex="-1" aria-labelledby="addModalLabel" aria-hidden="true" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1">
+            <div class="modal-dialog modal-xl modal-dialog-scrollable">
+                <div class="modal-content">
+                    <!-- Modal content will be loaded here -->
+                </div>
+            </div>
+        </div>
+
+        <!-- Detail modal container -->
+        <div class="modal fade" id="detailModal" tabindex="-1" aria-labelledby="detailModalLabel" aria-hidden="true" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1">
+            <div class="modal-dialog modal-xl modal-dialog-scrollable">
+                <div class="modal-content">
+                    <!-- Modal content will be loaded here -->
+                </div>
+            </div>
+        </div>
+
+        {{-- Edit Modal --}}
+        <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1">
+            <div class="modal-dialog modal-xl modal-dialog-scrollable">
+                <div class="modal-content">
+                    <!-- Modal content will be loaded here -->
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<script src="{{ asset('assets/jquery/dist/jquery.min.js') }}"></script>
+<script src="{{ asset('assets/DataTables/datatables.min.js') }}"></script>
+<script src="{{ asset('assets/DataTables/DataTables-1.13.6/js/dataTables.bootstrap5.min.js') }}"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script>
     function showLoader() {
         $('#loader-modal').css('display', 'flex');
@@ -180,10 +191,6 @@
                 }
             });
         });
-        $('#createModal').on('hidden.bs.modal', function() {
-            datatables.ajax.reload(function() {
-            }, false);
-        });
 
         // Reload data
         $('#reloadDatatable').on('click', function() {
@@ -226,10 +233,6 @@
                     alert('An error occurred while loading the view form.');
                 }
             });
-        });
-        $('#editModal').on('hidden.bs.modal', function() {
-            datatables.ajax.reload(function() {
-            }, false);
         });
 
         $('#data_trxs').on('click', '.delete-app-btn', function () {
@@ -283,9 +286,4 @@
         }
     });
 </script>
-@endpush
-@push('jsvendor')
-    {{-- <script src="{!! asset('assets') !!}/vendor/libs/datatables-bs5/datatables-bootstrap5.js"></script> --}}
-    <script src="{!! asset('assets/vendor/libs/datatables-bs5/datatables-bootstrap5.js') !!}"></script>
-    <script src="{!! asset('assets/libvelixs/client-dist/socket.io.js') !!}"></script>
-@endpush
+@endsection
